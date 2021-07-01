9d36968ff0e6ce91cb50b8a3b0de4323770323e6
zen-ap <78710266+zen-ap@users.noreply.github.com>
2021-07-01 00:47:54 -0400

    part2.1

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,7 +3,18 @@ const DaiToken = artifacts.require('DaiToken')
 const TokenFarm = artifacts.require('TokenFarm')
 
 module.exports = async function(deployer, network, accounts) {
-}
+  // Deploy Mock DAI Token
+  await deployer.deploy(DaiToken)
+  const daiToken = await DaiToken.deployed()
+
+  // Deploy Dapp Token
+  await deployer.deploy(DappToken)
+  const dappToken = await DappToken.deployed()
+
+  // Deploy TokenFarm
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
+  const tokenFarm = await TokenFarm.deployed()
+
   // Transfer all tokens to TokenFarm (1 million)
   await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
 
