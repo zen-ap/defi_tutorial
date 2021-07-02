d312b7ac4658bd89af67472dc31e8b3ac43cd382
zen-ap <78710266+zen-ap@users.noreply.github.com>
2021-07-02 14:39:11 -0400

    stake/issue tokens

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -7,9 +7,42 @@ contract TokenFarm {
     string public name = "Dapp Token Farm";
     DappToken public dappToken;
     DaiToken public daiToken;
+    address public owner;
+
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
 
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
+        owner = msg.sender;
     }
+
+    //Stake Tokens (deposit)
+    function stakeTokens(uint _amount) public {
+        require(_amount > 0, "amount cannot be 0");
+        daiToken.transferFrom(msg.sender, address(this), _amount);
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
+    }
+
+    //Issuing Tokens
+    function issueTokens() public {
+        require(msg.sender == owner, "caller must be owner");
+        for (uint i=0; i<stakers.length; i++) {
+            address recipient = stakers[i];
+            uint balance = stakingBalance[recipient];
+            dappToken.transfer(recipient, balance);
+        }
+    }
+
+    //Unstake Tokens (withdraw)
+
 }
